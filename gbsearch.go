3af492a056dc46405bcc4a97470906103c4478cf
Guy Oliver <guy.oliver@gmail.com>
2013-08-20 09:54:36 -0400

    updated file structure

--- gbsearch.go
+++ gbsearch.go
@@ -1,172 +1,18 @@
+// Package gbsearch provides a way to pragmatically search Google books api
 package gbsearch
 
 import (
 	"encoding/json"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"strconv"
 )
 
-type SearchType string
-
-const (
-	InTitle           SearchType = "intitle"
-	InAuthor          SearchType = "inauthor"
-	InPublisher       SearchType = "inpublisher"
-	Subject           SearchType = "subject"
-	ISBN              SearchType = "isbn"
-	LCCN              SearchType = "lccn"
-	OCLC              SearchType = "oclc"
-	UnknownSearchType SearchType = "unknown"
-)
-
-type FilterType byte
-type PrintType byte
-type ProjectionType byte
-type OrderType byte
-
-const (
-	UnknownFilterType FilterType = iota
-	PartialText
-	FullText
-	FreeEbooks
-	PaidEbooks
-	EBooks
-)
-
-func (this FilterType) String() string {
-	switch {
-	case this == PartialText:
-		return "partial"
-	case this == FullText:
-		return "full"
-	case this == FreeEbooks:
-		return "free-ebooks"
-	case this == PaidEbooks:
-		return "paid-ebooks"
-	case this == EBooks:
-		return "ebooks"
-	}
-	return "unknown"
-}
-
-const (
-	UnknownPrintType PrintType = iota
-	All
-	Books
-	Magazines
-)
-
-func (this PrintType) String() string {
-	switch {
-	case this == All:
-		return "all"
-	case this == Books:
-		return "books"
-	case this == Magazines:
-		return "magazines"
-	}
-	return "unknown"
-}
-
-const (
-	UnknownProjectionType ProjectionType = iota
-	FullResults
-	Lite
-)
-
-func (this ProjectionType) String() string {
-	switch {
-	case this == FullResults:
-		return "full"
-	case this == Lite:
-		return "lite"
-	}
-	return "unknown"
-}
-
-const (
-	UnknownOrderByType OrderType = iota
-	Relevance
-	Newest
-)
-
-func (this OrderType) String() string {
-	switch {
-	case this == Relevance:
-		return "relevance"
-	case this == Newest:
-		return "newest"
-	}
-	return "unknown"
-}
-
-type Options struct {
-	onlyEPubDownloads *bool
-	filter            *FilterType
-	startIndex        *int
-	maxResults        *int
-	printType         *PrintType
-	projection        *ProjectionType
-	orderBy           *OrderType
-	languageCode      *string
-}
-
-func DefaultOptions() *Options {
-	return &Options{}
-}
-
-func (this *Options) OnlyFindEPubDownloads(val bool) {
-	this.onlyEPubDownloads = &val
-}
-
-func (this *Options) SetFilter(ft FilterType) {
-	if ft != UnknownFilterType {
-		this.filter = &ft
-	}
-}
-
-func (this *Options) SetStartIndex(si int) {
-	if si > 0 {
-		this.startIndex = &si
-	}
-}
-
-func (this *Options) SetMaxResults(mr int) {
-	if mr > 0 {
-		this.maxResults = &mr
-	}
-}
-
-func (this *Options) SetPrintType(pt PrintType) {
-	if pt != UnknownPrintType {
-		this.printType = &pt
-	}
-}
-
-func (this *Options) SetProjection(p ProjectionType) {
-	if p != UnknownProjectionType {
-		this.projection = &p
-	}
-}
-
-func (this *Options) SetOrderBy(o OrderType) {
-	if o != UnknownOrderByType {
-		this.orderBy = &o
-	}
-}
-
-func (this *Options) SetLanguageCode(lc string) {
-	if lc != "" {
-		this.languageCode = &lc
-	}
-}
-
 // https://developers.google.com/books/docs/v1/using#PerformingSearch
-// need to expand to support all the options
 
+// Search will perform the given type of search on the given search term, using the provided
+// options. Options can be nil if you don't want to change anything from the defaults
 func Search(searchType SearchType, searchTerm string, options *Options) (*Results, error) {
 
 	u := &url.URL{
@@ -183,7 +29,7 @@ func Search(searchType SearchType, searchTerm string, options *Options) (*Result
 		}
 
 		if options.filter != nil {
-			query.Add("filter", options.filter.String())
+			query.Add("filter", string(*options.filter))
 		}
 
 		if options.languageCode != nil {
@@ -199,15 +45,15 @@ func Search(searchType SearchType, searchTerm string, options *Options) (*Result
 		}
 
 		if options.projection != nil {
-			query.Add("projection", options.projection.String())
+			query.Add("projection", string(*options.projection))
 		}
 
 		if options.printType != nil {
-			query.Add("printType", options.printType.String())
+			query.Add("printType", string(*options.printType))
 		}
 
 		if options.orderBy != nil {
-			query.Add("orderBy", options.orderBy.String())
+			query.Add("orderBy", string(*options.orderBy))
 		}
 	}
 
@@ -220,8 +66,6 @@ func Search(searchType SearchType, searchTerm string, options *Options) (*Result
 	u.RawQuery = query.Encode()
 
 	target := u.String()
-
-	log.Println(target)
 
 	resp, err := http.Get(target)
 	if err != nil {
@@ -243,30 +87,37 @@ func Search(searchType SearchType, searchTerm string, options *Options) (*Result
 	return res, nil
 }
 
+// TitleSearch will perform a title search on the given search term and with the given options
 func TitleSearch(searchTerm string, options *Options) (*Results, error) {
 	return Search(InTitle, searchTerm, options)
 }
 
+// AuthorSearch will perform an author search on the given search term and with the given options
 func AuthorSearch(searchTerm string, options *Options) (*Results, error) {
 	return Search(InAuthor, searchTerm, options)
 }
 
+// PublisherSearch will perform a publisher search on the given search term and with the given options
 func PublisherSearch(searchTerm string, options *Options) (*Results, error) {
 	return Search(InPublisher, searchTerm, options)
 }
 
+// SubjectSearch will perform a subject search on the given search term and with the given options
 func SubjectSearch(searchTerm string, options *Options) (*Results, error) {
 	return Search(Subject, searchTerm, options)
 }
 
+// ISBNSearch will perform a isbn search on the given search term and with the given options
 func ISBNSearch(searchTerm string, options *Options) (*Results, error) {
 	return Search(ISBN, searchTerm, options)
 }
 
+// LCCNSearch will perform a lccn search on the given search term and with the given options
 func LCCNSearch(searchTerm string, options *Options) (*Results, error) {
 	return Search(LCCN, searchTerm, options)
 }
 
+// OCLCSearch will perform a oclc number search on the given search term and with the given options
 func OCLCSearch(searchTerm string, options *Options) (*Results, error) {
 	return Search(OCLC, searchTerm, options)
 }
---
+++ options.go
@@ -0,0 +1,143 @@
+package gbsearch
+
+// SearchType is used to represent the various detailed kinds of searches we
+// can do.  Google Books api by default does a general search, but we can
+// constrain it to more specific searches.
+type SearchType string
+
+// These constants define the various kinds of searches we can do and will
+// be used later to specify the kind of search we want to do.
+const (
+	InTitle           SearchType = "intitle"
+	InAuthor          SearchType = "inauthor"
+	InPublisher       SearchType = "inpublisher"
+	Subject           SearchType = "subject"
+	ISBN              SearchType = "isbn"
+	LCCN              SearchType = "lccn"
+	OCLC              SearchType = "oclc"
+	UnknownSearchType SearchType = "unknown"
+)
+
+// FilterType is used to specify the kind of filtering we want to do to the search.
+type FilterType string
+
+// These constants are used to specify the filter type we want applied.  We can restrict the
+// searches to return books that have partial text for viewing, full text, that are ebooks,
+// either free or paid.
+const (
+	UnknownFilterType FilterType = "unknown"
+	PartialText       FilterType = "partial"
+	FullText          FilterType = "full"
+	FreeEbooks        FilterType = "free-ebooks"
+	PaidEbooks        FilterType = "paid-ebooks"
+	EBooks            FilterType = "ebooks"
+)
+
+// PrintType is used to specify which kind of media we are looking for.
+type PrintType string
+
+// These constants are used to specify the media type we want, books, magazines, or all.
+const (
+	UnknownPrintType PrintType = "unknown"
+	All              PrintType = "all"
+	Books            PrintType = "books"
+	Magazines        PrintType = "magazines"
+)
+
+// ProjectionType is used to specify how much data we want returned to us.
+type ProjectionType string
+
+// These constants are used to specify if we want full or lite levels of data.
+const (
+	UnknownProjectionType ProjectionType = "unknown"
+	FullResults           ProjectionType = "full"
+	Lite                  ProjectionType = "lite"
+)
+
+// OrderType is used to specify how to order the data.
+type OrderType string
+
+// These constants are used to specify if we want the data ordered by relevance or by newest.
+const (
+	UnknownOrderByType OrderType = "unknown"
+	Relevance          OrderType = "relevance"
+	Newest             OrderType = "newest"
+)
+
+// Options are used to collect all of our various option settings into one place to allow
+// them all to be passed into the various search functions at once.  Use the various
+// SetXXX functions to adjust its contents
+type Options struct {
+	onlyEPubDownloads *bool
+	filter            *FilterType
+	startIndex        *int
+	maxResults        *int
+	printType         *PrintType
+	projection        *ProjectionType
+	orderBy           *OrderType
+	languageCode      *string
+}
+
+// DefaultOptions returns a new Options struct set with the default values.  Currently, the default
+// is for none of the options to be set.
+func DefaultOptions() *Options {
+	return &Options{}
+}
+
+// OnlyFindEPubDownloads will adjust whether we are to search for all books or just
+// downloadable ePub books.
+func (this *Options) OnlyFindEPubDownloads(val bool) {
+	this.onlyEPubDownloads = &val
+}
+
+// SetFilter is used to set the filter type.
+func (this *Options) SetFilter(ft FilterType) {
+	if ft != UnknownFilterType {
+		this.filter = &ft
+	}
+}
+
+// SetStartIndex is used to set the start index.  In conjunction with SetMaxResults, we can fetch
+// pages of data from the server.
+func (this *Options) SetStartIndex(si int) {
+	if si > 0 {
+		this.startIndex = &si
+	}
+}
+
+// SetMaxResults is used to set the max number of results to fetch.  The max is 40.  In conjunction with
+// SetStartIndex, we can fetch pages of data from the server.
+func (this *Options) SetMaxResults(mr int) {
+	if mr > 0 {
+		this.maxResults = &mr
+	}
+}
+
+// SetPrintType is used to set the media type.
+func (this *Options) SetPrintType(pt PrintType) {
+	if pt != UnknownPrintType {
+		this.printType = &pt
+	}
+}
+
+// SetProjection is used to set how much data we want brought back with reach result.
+func (this *Options) SetProjection(p ProjectionType) {
+	if p != UnknownProjectionType {
+		this.projection = &p
+	}
+}
+
+// SetOrderBy is used to set how to order the data.
+func (this *Options) SetOrderBy(o OrderType) {
+	if o != UnknownOrderByType {
+		this.orderBy = &o
+	}
+}
+
+// SetLanguageCode will let us set which language code to restrict our search to.  Use the two leter
+// standard country code, such as "en", "fr", etc...
+func (this *Options) SetLanguageCode(lc string) {
+	if lc != "" {
+		this.languageCode = &lc
+	}
+}
